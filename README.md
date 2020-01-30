a9de09da36c673daadc2a6f8497c2f0866e91490
jbpratt78 <jbpratt78@gmail.com>
2020-01-29 19:00:17 -0600

    update readme and make things more readable

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Cleaning up default VPC resources on new accounts
+# AWS Default VPC Cleanup
+
+This cleans up existing default VPC resources on new accounts that are provisioned on creation. Simply executing the script will query out to the EC2 service (each region, one at a time) grabbing the default VPC ID and pass it along with every request made to the cli, ensuring we are deleting only resources associated it.
+
+Order of operations:
+1.) Delete the internet gateway
+2.) Delete subnets
+3.) Delete route tables
+4.) Delete network access lists
+5.) Delete security groups
+6.) Delete the VPC
+7.) Delete left over DHCP options
--- cleanup-default-resources.sh
+++ cleanup-default-resources.sh
@@ -3,13 +3,18 @@
 function delete_igw() {
 	local region=$1
 	local vpc=$2
-	igws=$(aws ec2 describe-internet-gateways --filters Name=attachment.vpc-id,Values="${vpc}" --region "${region}" | jq '.InternetGateways | .[] | .InternetGatewayId' | tr -d '"')
+	igws=$(aws ec2 describe-internet-gateways \
+    --filters Name=attachment.vpc-id,Values="${vpc}" \
+    --region "${region}" | jq '.InternetGateways | .[] | .InternetGatewayId' | tr -d '"')
 	if [ -n "$igws" ]; then
 		for igw_id in $igws; do
-			if aws ec2 detach-internet-gateway --internet-gateway-id "${igw_id}" --vpc-id "${vpc}" --region "${region}"; then
-				echo "detached ${igw_id} from ${vpc}"
-				aws ec2 delete-internet-gateway --internet-gateway-id "${igw_id}" --region "${region}"
-				echo "deleted ${igw_id}"
+			if aws ec2 detach-internet-gateway \
+          --internet-gateway-id "${igw_id}" \
+          --vpc-id "${vpc}" --region "${region}"; then
+
+				aws ec2 delete-internet-gateway \
+          --internet-gateway-id "${igw_id}" --region "${region}"
+				echo "detached ${igw_id} from ${vpc} and deleted"
 			fi
 		done
 	fi
@@ -19,7 +24,9 @@ function delete_igw() {
 function delete_subnets() {
 	local region=$1
 	local vpc=$2
-	subnets=$(aws ec2 describe-subnets --filters Name=vpc-id,Values="${vpc}" --region "${region}" | jq '.[] | .[].SubnetId' | tr -d '"')
+	subnets=$(aws ec2 describe-subnets \
+    --filters Name=vpc-id,Values="${vpc}" \
+    --region "${region}" | jq '.[] | .[].SubnetId' | tr -d '"')
 	for subnet_id in $subnets; do
 		aws ec2 delete-subnet --subnet-id "${subnet_id}" --region "${region}"
 		echo "deleted ${subnet_id}"
@@ -29,7 +36,10 @@ function delete_subnets() {
 function delete_route_tables() {
 	local region=$1
 	local vpc=$2
-	rts=$(aws ec2 describe-route-tables --filters Name=vpc-id,Values="${vpc}" --region "${region}" | jq '.RouteTables | .[].Associations | .[] | select(.Main != true) | .RouteTableId' | tr -d '"')
+	rts=$(aws ec2 describe-route-tables \
+    --filters Name=vpc-id,Values="${vpc}" \
+    --region "${region}" \
+    | jq '.RouteTables | .[].Associations | .[] | select(.Main != true) | .RouteTableId' | tr -d '"')
 	for rt_id in $rts; do
 		aws ec2 delete-route-table --route-table-id "${rt_id}" --region "${region}"
 		echo "deleted ${rt_id}"
@@ -39,7 +49,10 @@ function delete_route_tables() {
 function delete_acls() {
 	local region=$1
 	local vpc=$2
-	acls=$(aws ec2 describe-network-acls --filters Name=vpc-id,Values="${vpc}" --region "${region}" | jq '.NetworkAcls | .[] | select(.IsDefault != true) | .NetworkAclId' | tr -d '"')
+	acls=$(aws ec2 describe-network-acls \
+    --filters Name=vpc-id,Values="${vpc}" \
+    --region "${region}" \
+    | jq '.NetworkAcls | .[] | select(.IsDefault != true) | .NetworkAclId' | tr -d '"')
 	for acl_id in $acls; do
 		aws ec2 delete-network-acl --network-acl-id "${acl_id}" --region "${region}"
 		echo "deleted ${acl_id}"
@@ -49,7 +62,10 @@ function delete_acls() {
 function delete_security_groups() {
 	local region=$1
 	local vpc=$2
-	sgps=$(aws ec2 describe-security-groups --filters Name=vpc-id,Values="${vpc}" --region "${region}" | jq '.SecurityGroups | .[] | select(.GroupName != "default") | .GroupId' | tr -d '"')
+	sgps=$(aws ec2 describe-security-groups \
+    --filters Name=vpc-id,Values="${vpc}" \
+    --region "${region}" \
+    | jq '.SecurityGroups | .[] | select(.GroupName != "default") | .GroupId' | tr -d '"')
 	for sgp_id in $sgps; do
 		aws ec2 delete-security-group --group-id "${sgp_id}" --region "${region}"
 		echo "deleted ${sgp_id}"
@@ -58,7 +74,9 @@ function delete_security_groups() {
 
 function delete_dhcp_options() {
 	local region=$1
-	dhcp_ops=$(aws ec2 describe-dhcp-options --filters "Name=key,Values=domain-name-servers,Name=value,Values=AmazonProvidedDNS" --region "${region}" | jq '.DhcpOptions | .[] | .DhcpOptionsId' | tr -d '"')
+	dhcp_ops=$(aws ec2 describe-dhcp-options \
+    --filters "Name=key,Values=domain-name-servers,Name=value,Values=AmazonProvidedDNS" \
+    --region "${region}" | jq '.DhcpOptions | .[] | .DhcpOptionsId' | tr -d '"')
 	for dhcp_ops_id in $dhcp_ops; do
 		aws ec2 delete-dhcp-options --dhcp-options-id "${dhcp_ops_id}" --region "${region}"
 		echo "deleted ${dhcp_ops_id}"
@@ -72,12 +90,11 @@ function delete_vpc() {
 	echo "deleted ${vpc}"
 }
 
-regions=$(aws ec2 describe-regions | jq '.[] | .[].RegionName' | tr -d '"')
-
 if [[ $(aws sts get-caller-identity 2> >(grep -c 'invalid')) -eq 1 ]]; then
 	exit
 fi
 
+regions=$(aws ec2 describe-regions | jq '.[] | .[].RegionName' | tr -d '"')
 for region in $regions; do
 	echo "starting ${region}..."
 	vpc_id=$(aws ec2 describe-account-attributes --attribute-name default-vpc \
@@ -86,7 +103,6 @@ for region in $regions; do
 	if [ "${vpc}" == "none" ]; then
 		echo "no default vpc to delete"
 		echo "moving on..."
-
 		echo "attempting to delete left over DHCP options for good measure"
 		delete_dhcp_options "${region}" "${vpc_id}"
 	fi
@@ -105,11 +121,3 @@ for region in $regions; do
 	echo "deleting left over DHCP options"
 	delete_dhcp_options "${region}" "${vpc_id}"
 done
-
-# get account atrributes
-#  1.) Delete the internet gateway
-#  2.) Delete subnets
-#  3.) Delete route tables
-#  4.) Delete network access lists
-#  5.) Delete security groups
-#  6.) Delete the VPC
